c2f0e0fb8600bec9c3ef6fc79dab7c4418cb4d10
Valdir Junior <valdir.junior@fernride.com>
2025-04-14 11:45:16 +0200

    updating files npm

--- webpage/node_modules/next/dist/server/lib/server-ipc/utils.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const ipcForbiddenHeaders: string[];
-export declare const actionsForbiddenHeaders: string[];
-export declare const filterReqHeaders: (headers: Record<string, undefined | string | number | string[]>, forbiddenHeaders: string[]) => Record<string, string | string[] | undefined>;
-export declare const filterInternalHeaders: (headers: Record<string, undefined | string | string[]>) => void;
--- webpage/node_modules/next/dist/shared/lib/match-remote-pattern.d.ts
+++
@@ -1,3 +0,0 @@
-import type { RemotePattern } from './image-config';
-export declare function matchRemotePattern(pattern: RemotePattern, url: URL): boolean;
-export declare function hasRemoteMatch(domains: string[], remotePatterns: RemotePattern[], url: URL): boolean;
