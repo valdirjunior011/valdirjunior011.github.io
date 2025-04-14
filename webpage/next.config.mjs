04f2552482433bb3c17ce9e10cf3ad935d450c8b
Valdir Junior <valdir.junior@fernride.com>
2025-04-14 15:05:28 +0200

    update actions file

--- webpage/next.config.mjs
+++ webpage/next.config.mjs
@@ -1,11 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? "/webpage" : "",
-  // Replace 'webpage' with your repository name
 };
-
-export default nextConfig;
